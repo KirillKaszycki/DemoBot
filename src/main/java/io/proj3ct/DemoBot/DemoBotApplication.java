afe5cea2b0d40dcc0c3367f9da75247ce21f16d2
KirillKaszycki <kashitskii.kirill@gmail.com>
2025-02-07 15:58:03 +0500

    initial commit

---
+++ src/main/java/io/proj3ct/DemoBot/DemoBotApplication.java
@@ -0,0 +1,14 @@
+package io.proj3ct.DemoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBotApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoBotApplication.class, args);
+	}
+
+}
